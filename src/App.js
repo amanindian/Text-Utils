d5102084b54157dc3db0a03b9f7af00575025f9b
Aman Kumar <101081368+amanindian@users.noreply.github.com>
2023-02-16 19:10:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import './App.css';
-import Navbar from './Components/NavBar.js'
-import TextForm from './Components/TextForm.js'
+import Navbar from './Components/Navbar.js'
+import Alert from './Components/Alert.js'
+import Textform from './Components/Textform.js'
 import React, { useState } from 'react'
 
 
 
 function App() {
+  //State for Light and Dark Mode
   const [Mode, setMode] = useState(`light`)
   const onModef = () => {
     if (Mode === `light`) {
       setMode(`dark`);
       document.body.style.setProperty('background-color', '#1c1d62', 'important');
+      Showpopup("Dark Mode Enabled", "success")
     } else {
       setMode(`light`)
       document.body.style.setProperty('background-color', 'white', 'important');
+      Showpopup("Light Mode Enabled", "success")
     }
+  }
+
+  //Control Dismissible Popup/Alert
+  const [Popup, setPopup] = useState(null)
+  const Showpopup = (message, type = "success") => {
+    setPopup({
+      type: type,
+      msg: message
+    })
+    setTimeout(() => {
+      setPopup(null)
+    }, 1500)
   }
 
   return (
     <>
       <Navbar title="NavaBar" Mode={Mode} onMode={onModef} />
-      <TextForm heading="Enter the text to analyze below" Mode={Mode} />
+      <Alert Popup={Popup} />
+      <Textform Showpopup={Showpopup} heading="Enter the text to analyze below" Mode={Mode} />
     </>
   );
 }
 export default App;
-
